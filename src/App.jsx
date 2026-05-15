a703c3f118bfff52176a9ee03dd4356c917eb56b
Suraj Yadav <surajyadav44942@gmail.com>
2026-05-15 21:07:50 +0530

    authentication is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,63 @@
+import { useState } from 'react'
 import './App.css'
 import { supabase } from './supabase'
 
 function App() {
 
-  console.log(supabase)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [note, setNote] = useState('')
+
+  async function saveNote() {
+
+    const { data, error } = await supabase
+      .from('notes')
+      .insert([
+        { content: note }
+      ])
+
+    if(error){
+      alert(error.message)
+    } else {
+      alert('Note saved!')
+      console.log(data)
+    }
+  }
 
   return (
-    <h1>StudyHub Connected 🚀</h1>
+    <div>
+
+      <h1>StudyHub</h1>
+
+      <input
+        type="email"
+        placeholder="Enter email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <br /><br />
+
+      <input
+        type="password"
+        placeholder="Enter password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <br /><br />
+
+      <input
+        type="text"
+        placeholder="Enter note"
+        onChange={(e) => setNote(e.target.value)}
+      />
+
+      <br /><br />
+
+      <button onClick={saveNote}>
+        Save Note
+      </button>
+
+    </div>
   )
 }
 
