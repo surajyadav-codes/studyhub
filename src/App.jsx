69935fa62047f0135542424ebaf8920fb8cff3c9
Suraj Yadav <surajyadav44942@gmail.com>
2026-05-15 11:28:27 +0530

    studyhub setup complete

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { supabase } from './supabase'
+
+function App() {
+
+  console.log(supabase)
+
+  return (
+    <h1>StudyHub Connected 🚀</h1>
+  )
+}
+
+export default App
---
+++ src/supabase.js
@@ -0,0 +1,9 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://sgwdsilzvvbipvzvlucd.supabase.co/rest/v1/'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNnd2RzaWx6dnZiaXB2enZsdWNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4MjI2OTgsImV4cCI6MjA5NDM5ODY5OH0.hE1Gt7IrWnoeKgNMa-ThCxp0wVC3_Wf7h8Rb4fdYRMM'
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseKey
+)
